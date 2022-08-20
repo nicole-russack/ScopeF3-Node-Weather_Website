40a00226d9e019b3b4cfeba5ce07e3be1718eae0
nicole-russack <russack@usc.edu>
2022-08-20 11:13:06 -0400

    Changed to add to heroku

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ weatherFrom.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error){
             //console.log(data.error)
